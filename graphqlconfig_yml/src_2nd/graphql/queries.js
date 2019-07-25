d4658252211362d35296110fdef8b19b507d4e08
thinkAmi <dev.thinkami@gmail.com>
2019-07-25 23:17:54 +0900

    add graphqlconfig_yml sample (2nd commit)

---
+++ graphqlconfig_yml/src_2nd/graphql/queries.js
@@ -0,0 +1,24 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getMyType = `query GetMyType($id: ID!) {
+  getMyType(id: $id) {
+    id
+    title
+  }
+}
+`;
+export const listMyTypes = `query ListMyTypes(
+  $filter: ModelMyTypeFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listMyTypes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+    }
+    nextToken
+  }
+}
+`;
---
+++ graphqlconfig_yml/src_2nd/graphql/subscriptions.js
@@ -0,0 +1,24 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMyType = `subscription OnCreateMyType {
+  onCreateMyType {
+    id
+    title
+  }
+}
+`;
+export const onUpdateMyType = `subscription OnUpdateMyType {
+  onUpdateMyType {
+    id
+    title
+  }
+}
+`;
+export const onDeleteMyType = `subscription OnDeleteMyType {
+  onDeleteMyType {
+    id
+    title
+  }
+}
+`;
