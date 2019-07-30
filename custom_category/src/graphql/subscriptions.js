2e6b432c16a7760fd510cc3a73656208db9508f0
thinkAmi <dev.thinkami@gmail.com>
2019-07-30 22:37:55 +0900

    add custom_category sample

---
+++ custom_category/src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMyType = `subscription OnCreateMyType {
+  onCreateMyType {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
+export const onUpdateMyType = `subscription OnUpdateMyType {
+  onUpdateMyType {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
+export const onDeleteMyType = `subscription OnDeleteMyType {
+  onDeleteMyType {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
